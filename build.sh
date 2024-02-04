abfb8a6e7a9d358301c2edd655ed2d9308f9f086
Taleh Ibrahimli <talehsmail@gmail.com>
2024-02-04 15:52:50 +0400

    +fixes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-npm install
-npm run build
+/root/.nvm/versions/node/v20.6.1/bin/npm install
+/root/.nvm/versions/node/v20.6.1/bin/npm run build
 
 docker build -f Dockerfile . -t docker-registry.apibrew.io/studio:latest --platform linux/amd64 || exit 1
 
