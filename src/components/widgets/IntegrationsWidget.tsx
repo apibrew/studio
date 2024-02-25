74fb78729573d7e39a2c11a8a7e7a9f22908542d
Taleh Ibrahimli <talehsmail@gmail.com>
2024-02-26 01:56:20 +0400

    +fixes

--- src/components/widgets/IntegrationsWidget.tsx
+++ src/components/widgets/IntegrationsWidget.tsx
@@ -20,7 +20,7 @@ export function IntegrationsWidget() {
                 </Typography>
                 <Box display='flex'>
                     <code>
-                        apbr configure --cloud --project {connection.name} --token {connection.serverConfig.authentication.token}
+                        apbr configure --cloud --project {connection.name} --token &lt;token&gt;
                     </code>
                     <Box flexGrow={1}/>
                     <Button
@@ -30,8 +30,8 @@ export function IntegrationsWidget() {
                             display: 'inline-block'
                         }}
                         onClick={() => {
-                            navigator.clipboard.writeText('apbr configure --cloud --project {connection.name} --token {connection.serverConfig.authentication.token}')
-                            toast.success('Token copied to clipboard')
+                            navigator.clipboard.writeText(`apbr configure --cloud --project ${connection.name} --token ${connection.serverConfig.authentication.token}`)
+                            toast.success('Copied to clipboard')
                         }}>Copy to clipboard</Button>
                 </Box>
                 <Typography>Additional configuration options:</Typography>
