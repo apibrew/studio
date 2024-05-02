db0f27650c6da85b67a1882cf963c4e192778b46
Taleh Ibrahimli <talehsmail@gmail.com>
2024-05-02 15:48:41 +0400

    +fixes

--- deployr.sh
+++ deployr.sh
@@ -1,4 +1,9 @@
 git add . -A
 git commit -m '+fixes'
 git push
-ssh root@tisserv.net "cd studio; git pull; sh build.sh"
+
+# Determine current Git branch
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+
+ssh root@tisserv.net "cd studio; git checkout $BRANCH; git pull; sh build.sh"
