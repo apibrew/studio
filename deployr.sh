97377278be4fdcb09613c13f338f5d3ea3a1d0a5
Taleh Ibrahimli <talehsmail@gmail.com>
2024-05-06 15:11:55 +0400

    add auto deploy

--- deployr.sh
+++
@@ -1,9 +0,0 @@
-git add . -A
-git commit -m '+fixes'
-git push
-
-# Determine current Git branch
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-
-ssh root@tisserv.net "cd studio; git checkout package-lock.json; git checkout $BRANCH; git pull; sh build.sh"
