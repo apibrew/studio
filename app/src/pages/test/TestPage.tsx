e3be5f7a51761da86b395675f812d494e2abba40
Taleh Ibrahimli <talehsmail@gmail.com>
2024-05-10 22:49:23 +0400

    setup docker build

--- app/src/pages/test/TestPage.tsx
+++ app/src/pages/test/TestPage.tsx
@@ -1,9 +1,11 @@
-import {helloAnything} from "core";
+import {helloAnything, TestXPage} from "core";
 
 export function TestPage() {
     const data = helloAnything("world");
 
     return <div>
         {data}
+
+        <TestXPage/>
     </div>
 }
