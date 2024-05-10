ac0f17605198f737eb7901d3defccb4c13c6d467
Taleh Ibrahimli <talehsmail@gmail.com>
2024-05-10 19:36:09 +0400

    setup core module

--- app/src/pages/test/TestPage.tsx
+++ app/src/pages/test/TestPage.tsx
@@ -1,26 +1,9 @@
-import {useState} from "react";
-import {TextField} from "@mui/material";
+import {helloAnything} from "core";
 
 export function TestPage() {
-    const [remoteUrl, setRemoteUrl] = useState<string>('http://localhost:4173/assets/remoteEntry.js')
-    const [remoteName, setRemoteName] = useState<string>('List')
-    const [ModuleX, setModuleX] = useState<any>(null)
-
-    const loadRemote = () => {
-        const entry = import(/* @vite-ignore */remoteUrl)
-        entry.then((container) => {
-            container.get('./' + remoteName).then(setModuleX)
-        })
-    }
+    const data = helloAnything("world");
 
     return <div>
-        <TextField label="Remote URL" value={remoteUrl} onChange={(e) => setRemoteUrl(e.target.value)}/>
-        <TextField label="Remote Name" value={remoteName} onChange={(e) => setRemoteName(e.target.value)}/>
-        <button onClick={loadRemote}>Load Remote</button>
-
-        <div>
-            {ModuleX && <ModuleX items={['a', 'b']}/>}
-        </div>
-
+        {data}
     </div>
 }
