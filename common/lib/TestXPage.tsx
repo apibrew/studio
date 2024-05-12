5171a2b8a0bfc864a8b6decae31941c77cc7e020
Taleh Ibrahimli <talehsmail@gmail.com>
2024-05-12 18:04:54 +0400

    +fixes

--- common/lib/TestXPage.tsx
+++ common/lib/TestXPage.tsx
@@ -1,13 +1,7 @@
-import {container, PageComponentType} from "core";
+import {container} from "core";
 
 export function TestXPage() {
     return <div>Hello TestXPage ASD mmm!</div>;
 }
-
-container.registerComponent<PageComponentType>({
-    componentType: 'page',
-    name: 'TestXPage',
-    component: TestXPage,
-}, 'TestXPage')
 
 console.log(container)
