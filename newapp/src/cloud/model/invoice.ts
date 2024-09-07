97221fe93b8ce418c5278f64913b6c73808c5ce4
Taleh Ibrahimli <talehsmail@gmail.com>
2024-09-07 23:32:47 +0400

    +fixes

--- newapp/src/cloud/model/invoice.ts
+++ newapp/src/cloud/model/invoice.ts
@@ -1,20 +1,20 @@
+import {AccountPlan} from './account-plan';
 import {Payment} from './payment';
-import {Instance} from './instance';
-import {InstancePlan} from './instance-plan';
 
 export interface Invoice {
-    id: string
-    currency: string
     user: string
+    currency: string
     amount: number
+    version: number
     status: Status
+    plan: AccountPlan
     notes?: string
     payment?: Payment
     auditData?: AuditData
     executionStatus: ExecutionStatus
     paymentDate?: string
-    items: InvoiceItem[]
-    version: number
+    id: string
+    monthCount: number
 }
 
 export const InvoiceEntityInfo = {
@@ -23,19 +23,11 @@ export const InvoiceEntityInfo = {
     restPath: "invoice",
 }
 
-export interface InvoiceItem {
-    instance: Instance
-    monthCount: number
-    instancePreviousPlanName: string
-    instancePreviousPlanUntil: string
-    plan: InstancePlan
-}
-
 export interface AuditData {
-    updatedBy: string
-    updatedOn: string
     createdBy: string
     createdOn: string
+    updatedBy: string
+    updatedOn: string
 }
 
 export enum Status {
@@ -51,194 +43,229 @@ export enum ExecutionStatus {
 }
 
 export const InvoiceResource = {
-  "auditData": {
-    "createdBy": "admin",
-    "updatedBy": "system",
-    "createdOn": "2024-01-06T21:56:20Z",
-    "updatedOn": "2024-04-12T19:16:01Z"
-  },
-  "name": "Invoice",
-  "namespace": {
-    "name": "default"
-  },
-  "properties": {
-    "amount": {
-      "type": "INT32",
-      "required": true,
-      "description": "The amount of the payment"
-    },
     "auditData": {
-      "type": "STRUCT",
-      "typeRef": "AuditData",
-      "exampleValue": {
         "createdBy": "admin",
-        "createdOn": "2024-04-12T19:16:01Z",
         "updatedBy": "admin",
-        "updatedOn": "2024-04-12T19:16:01Z"
-      },
-      "title": "Audit Data",
-      "description": "The audit data of the resource/record. \nIt contains information about who created the resource/record, when it was created, who last updated the resource/record and when it was last updated.",
-      "annotations": {
-        "SpecialProperty": "true"
-      }
+        "createdOn": "2024-01-06T21:56:20Z",
+        "updatedOn": "2024-09-07T19:25:29Z"
     },
-    "currency": {
-      "type": "STRING",
-      "required": true,
-      "description": "The currency of the payment"
+    "name": "Invoice",
+    "namespace": {
+        "name": "default"
     },
-    "executionStatus": {
-      "type": "ENUM",
-      "required": true,
-      "defaultValue": "PENDING",
-      "enumValues": [
-        "PENDING",
-        "EXECUTING",
-        "EXECUTED",
-        "FAILED"
-      ]
-    },
-    "id": {
-      "type": "UUID",
-      "primary": true,
-      "required": true,
-      "immutable": true,
-      "exampleValue": "a39621a4-6d48-11ee-b962-0242ac120002",
-      "description": "The unique identifier of the resource. It is randomly generated and immutable.",
-      "annotations": {
-        "SpecialProperty": "true"
-      }
-    },
-    "items": {
-      "type": "LIST",
-      "required": true,
-      "item": {
-        "type": "STRUCT",
-        "typeRef": "InvoiceItem"
-      }
-    },
-    "notes": {
-      "type": "STRING",
-      "length": 1024,
-      "description": "The notes of the payment"
-    },
-    "payment": {
-      "type": "REFERENCE",
-      "reference": "default/Payment",
-      "description": "The payment of the payment"
-    },
-    "paymentDate": {
-      "type": "TIMESTAMP",
-      "description": "The date of the payment"
-    },
-    "status": {
-      "type": "ENUM",
-      "required": true,
-      "defaultValue": "PENDING",
-      "enumValues": [
-        "PENDING",
-        "PAID"
-      ]
-    },
-    "user": {
-      "type": "STRING",
-      "required": true,
-      "description": "The user of the payment"
-    },
-    "version": {
-      "type": "INT32",
-      "required": true,
-      "defaultValue": 1,
-      "exampleValue": 1,
-      "title": "Version",
-      "description": "The version of the resource/record. It is incremented on every update.",
-      "annotations": {
-        "AllowEmptyPrimitive": "true",
-        "SpecialProperty": "true"
-      }
-    }
-  },
-  "types": [
-    {
-      "name": "InvoiceItem",
-      "title": "",
-      "description": "",
-      "properties": {
-        "instance": {
-          "type": "REFERENCE",
-          "required": true,
-          "reference": "default/Instance",
-          "description": "The instance of the payment"
+    "properties": {
+        "amount": {
+            "type": "INT32",
+            "required": true,
+            "description": "The amount of the payment",
+            "annotations": {
+                "SourceMatchKey": "d94c368606d9"
+            }
         },
-        "instancePreviousPlanName": {
-          "type": "STRING"
+        "auditData": {
+            "type": "STRUCT",
+            "typeRef": "AuditData",
+            "exampleValue": {
+                "createdBy": "admin",
+                "createdOn": "2024-04-12T19:16:01Z",
+                "updatedBy": "admin",
+                "updatedOn": "2024-04-12T19:16:01Z"
+            },
+            "title": "Audit Data",
+            "description": "The audit data of the resource/record. \nIt contains information about who created the resource/record, when it was created, who last updated the resource/record and when it was last updated.",
+            "annotations": {
+                "SourceMatchKey": "7f1da8dc32fa",
+                "SpecialProperty": "true"
+            }
         },
-        "instancePreviousPlanUntil": {
-          "type": "TIMESTAMP"
+        "currency": {
+            "type": "STRING",
+            "required": true,
+            "description": "The currency of the payment",
+            "annotations": {
+                "SourceMatchKey": "9a5d49e89eaa"
+            }
+        },
+        "executionStatus": {
+            "type": "ENUM",
+            "required": true,
+            "defaultValue": "PENDING",
+            "enumValues": [
+                "PENDING",
+                "EXECUTING",
+                "EXECUTED",
+                "FAILED"
+            ],
+            "annotations": {
+                "SourceMatchKey": "4c60a2bb6bcd"
+            }
+        },
+        "id": {
+            "type": "UUID",
+            "primary": true,
+            "required": true,
+            "immutable": true,
+            "exampleValue": "a39621a4-6d48-11ee-b962-0242ac120002",
+            "description": "The unique identifier of the resource. It is randomly generated and immutable.",
+            "annotations": {
+                "SourceMatchKey": "182097aa9c1f",
+                "SpecialProperty": "true"
+            }
         },
         "monthCount": {
-          "type": "INT32",
-          "required": true,
-          "description": "The amount of the payment"
+            "type": "INT32",
+            "required": true,
+            "defaultValue": 1,
+            "annotations": {
+                "SourceMatchKey": "333079f2a701"
+            }
+        },
+        "notes": {
+            "type": "STRING",
+            "length": 1024,
+            "description": "The notes of the payment",
+            "annotations": {
+                "SourceMatchKey": "9022239993f3"
+            }
+        },
+        "payment": {
+            "type": "REFERENCE",
+            "reference": "default/Payment",
+            "description": "The payment of the payment",
+            "annotations": {
+                "SourceMatchKey": "b81652508bd4"
+            }
+        },
+        "paymentDate": {
+            "type": "TIMESTAMP",
+            "description": "The date of the payment",
+            "annotations": {
+                "SourceMatchKey": "e949f3e334ea"
+            }
         },
         "plan": {
-          "type": "REFERENCE",
-          "required": true,
-          "reference": "default/InstancePlan",
-          "description": "The plan of the payment"
+            "type": "REFERENCE",
+            "required": true,
+            "reference": "default/AccountPlan",
+            "annotations": {
+                "SourceMatchKey": "d9d500e2ead2"
+            }
+        },
+        "status": {
+            "type": "ENUM",
+            "required": true,
+            "defaultValue": "PENDING",
+            "enumValues": [
+                "PENDING",
+                "PAID"
+            ],
+            "annotations": {
+                "SourceMatchKey": "c9c9a5e7ee9b"
+            }
+        },
+        "user": {
+            "type": "STRING",
+            "required": true,
+            "description": "The user of the payment",
+            "annotations": {
+                "SourceMatchKey": "e5894cabaed7"
+            }
+        },
+        "version": {
+            "type": "INT32",
+            "required": true,
+            "defaultValue": 1,
+            "exampleValue": 1,
+            "title": "Version",
+            "description": "The version of the resource/record. It is incremented on every update.",
+            "annotations": {
+                "AllowEmptyPrimitive": "true",
+                "SourceMatchKey": "f8cc6f947f15",
+                "SpecialProperty": "true"
+            }
         }
-      }
     },
-    {
-      "name": "AuditData",
-      "title": "Audit Data",
-      "description": "Audit Data is a type that represents the audit data of a resource/record. ",
-      "properties": {
-        "createdBy": {
-          "type": "STRING",
-          "immutable": true,
-          "length": 256,
-          "exampleValue": "admin",
-          "title": "Created By",
-          "description": "The user who created the resource/record.",
-          "annotations": {
-            "SpecialProperty": "true"
-          }
+    "types": [
+        {
+            "name": "InvoiceItem",
+            "title": "",
+            "description": "",
+            "properties": {
+                "instance": {
+                    "type": "REFERENCE",
+                    "required": true,
+                    "reference": "default/Instance",
+                    "description": "The instance of the payment"
+                },
+                "instancePreviousPlanName": {
+                    "type": "STRING"
+                },
+                "instancePreviousPlanUntil": {
+                    "type": "TIMESTAMP"
+                },
+                "monthCount": {
+                    "type": "INT32",
+                    "required": true,
+                    "description": "The amount of the payment"
+                },
+                "plan": {
+                    "type": "REFERENCE",
+                    "required": true,
+                    "reference": "default/InstancePlan",
+                    "description": "The plan of the payment"
+                }
+            }
         },
-        "createdOn": {
-          "type": "TIMESTAMP",
-          "immutable": true,
-          "exampleValue": "2024-04-12T19:16:01Z",
-          "title": "Created On",
-          "description": "The timestamp when the resource/record was created.",
-          "annotations": {
-            "SpecialProperty": "true"
-          }
-        },
-        "updatedBy": {
-          "type": "STRING",
-          "length": 256,
-          "exampleValue": "admin",
-          "title": "Updated By",
-          "description": "The user who last updated the resource/record.",
-          "annotations": {
-            "SpecialProperty": "true"
-          }
-        },
-        "updatedOn": {
-          "type": "TIMESTAMP",
-          "exampleValue": "2024-04-12T19:16:01Z",
-          "title": "Updated On",
-          "description": "The timestamp when the resource/record was last updated.",
-          "annotations": {
-            "SpecialProperty": "true"
-          }
+        {
+            "name": "AuditData",
+            "title": "Audit Data",
+            "description": "Audit Data is a type that represents the audit data of a resource/record. ",
+            "properties": {
+                "createdBy": {
+                    "type": "STRING",
+                    "immutable": true,
+                    "length": 256,
+                    "exampleValue": "admin",
+                    "title": "Created By",
+                    "description": "The user who created the resource/record.",
+                    "annotations": {
+                        "SpecialProperty": "true"
+                    }
+                },
+                "createdOn": {
+                    "type": "TIMESTAMP",
+                    "immutable": true,
+                    "exampleValue": "2024-04-12T19:16:01Z",
+                    "title": "Created On",
+                    "description": "The timestamp when the resource/record was created.",
+                    "annotations": {
+                        "SpecialProperty": "true"
+                    }
+                },
+                "updatedBy": {
+                    "type": "STRING",
+                    "length": 256,
+                    "exampleValue": "admin",
+                    "title": "Updated By",
+                    "description": "The user who last updated the resource/record.",
+                    "annotations": {
+                        "SpecialProperty": "true"
+                    }
+                },
+                "updatedOn": {
+                    "type": "TIMESTAMP",
+                    "exampleValue": "2024-04-12T19:16:01Z",
+                    "title": "Updated On",
+                    "description": "The timestamp when the resource/record was last updated.",
+                    "annotations": {
+                        "SpecialProperty": "true"
+                    }
+                }
+            }
         }
-      }
+    ],
+    "annotations": {
+        "EnableAudit": "true"
     }
-  ],
-  "annotations": {
-    "EnableAudit": "true"
-  }
 } as unknown
 
