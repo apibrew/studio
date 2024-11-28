45fc4bb3ef4ad8f47da38735f07358a623ca161e
Taleh Ibrahimli <talehsmail@gmail.com>
2024-11-28 16:54:14 +0100

    +fixes

--- newapp/src/dashboard/components/api-doc/ApiDocModal.tsx
+++ newapp/src/dashboard/components/api-doc/ApiDocModal.tsx
@@ -1,18 +1,30 @@
 import {Resource, useClient} from "@apibrew/react";
-import {Box} from "@mui/material";
+import {Box, IconButton} from "@mui/material";
 import {useState} from "react";
 import {LoadingOverlay} from "common";
+import {Close} from "@mui/icons-material";
 
 export interface ApiDocModalProps {
     onClose: () => void
     resource?: Resource
 }
 
-export function ApiDocModal(_: ApiDocModalProps) {
+export function ApiDocModal(props: ApiDocModalProps) {
     const client = useClient()
     const [loading, setLoading] = useState(true)
 
     return <Box width={'1000px'}>
+        <Box display='flex' justifyContent='space-between' m={1} >
+            <div
+                className="fnt-600-20-Inter clr101828">Api Documentation {props.resource?.name}</div>
+            <IconButton onClick={() => {
+                if (props.onClose) {
+                    props.onClose()
+                }
+            }}>
+                <Close/>
+            </IconButton>
+        </Box>
         {loading && <LoadingOverlay/>}
         <iframe
             onLoad={() => setLoading(false)}
