b2b31961a3edf10a4544ba5230c0057a1f1f9a91
Taleh Ibrahimli <talehsmail@gmail.com>
2024-11-29 22:43:12 +0100

    +fixes

--- newapp/src/dashboard/components/help/HelpButton.tsx
+++ newapp/src/dashboard/components/help/HelpButton.tsx
@@ -1,20 +1,43 @@
 import Button from "@mui/material/Button";
 import {Help} from "@mui/icons-material";
 import {useState} from "react";
+import {Box, Popover, Tooltip} from "@mui/material";
 
 export function HelpButton() {
     const [helpAnchor, setHelpAnchor] = useState<HTMLElement>();
 
     return <>
-        <Button size='medium'
-                variant='outlined'
-                color={'success'}
-                onClick={(event) => {
-                    setHelpAnchor(event.currentTarget);
+        <Tooltip title='Show help materials for this page.'>
+            <Button size='medium'
+                    variant='outlined'
+                    color={'success'}
+                    onClick={(event) => {
+                        setHelpAnchor(event.currentTarget);
 
-                }}>
-            <Help fontSize='small'/>
-            <span style={{marginLeft: '3px'}}>Help</span>
-        </Button>
+                    }}>
+                <Help fontSize='small'/>
+                <span style={{marginLeft: '3px'}}>Help</span>
+            </Button>
+        </Tooltip>
+        <Popover
+            open={Boolean(helpAnchor)}
+            anchorEl={helpAnchor}
+            onClose={() => setHelpAnchor(undefined)}
+            anchorPosition={{
+                top: 200,
+                left: 200
+            }}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+            }}
+        >
+            <Box m={2}
+                 width='500px'
+                 height='400px'>
+                <a target='_blank'
+                   href='https://www.google.com'>How to create first API?</a>
+            </Box>
+        </Popover>
     </>
 }
